41202f02aed9889cc2d62d1c42036a8d16d4cbbd
Affan Mustafa <affanmustafa2297@gmail.com>
2024-09-14 22:27:39 +0500

    guessing_game

--- rust-book/guessing_game_learn/src/main.rs
+++ rust-book/guessing_game_learn/src/main.rs
@@ -1,44 +1,30 @@
 use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
-fn main() {
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-    let mut count: u8 = 0;
-    println!("Guess the number!");
 
-    println!("Please enter your guess");
+fn main() {
+    println!("Guess the number between 1 and 100");
+
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
 
     loop {
-        let mut guess: String = String::new();
-        io::stdin()
-            .read_line(&mut guess) // &mut is a mutable reference. read_line appends the input to the string that's why we use a mutable reference
-            .expect("Failed to read line");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Error");
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => {
-                println!("Please enter a valid number");
-                continue;
-            }
+            Err(_) => continue,
         };
 
         println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
-            Ordering::Less => {
-                println!("Too small!");
-                count += 1;
-            }
-            Ordering::Greater => {
-                println!("Too big!");
-                count += 1;
-            }
             Ordering::Equal => {
-                println!("You win!");
-                count += 1;
-                println!("You took {} guesses", count);
+                println!("You found it!");
                 break;
             }
+            Ordering::Greater => println!("Go lower"),
+            Ordering::Less => println!("Go higher"),
         }
     }
 }
